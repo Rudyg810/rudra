16b8df8c4f01c9da8411f716fdf65cb4c16cf24e
rudy8101 <rudra@createstir.com>
2024-09-27 19:29:41 +0530

    :

--- src/app/influencer-ai/components/breaker.jsx
+++ src/app/influencer-ai/components/breaker.jsx
@@ -5,7 +5,7 @@ export const Breaker = () => {
     <div className="fixed  z-30 w-full h-full top-0 right-0 backdrop-blur-lg flex items-center justify-center">
       <div className='block text-center'>
       <h1 className=" italic bg-gradient-to-r font-extrabold from-pink-700 to-purple-500 text-transparent bg-clip-text text-5xl sm:text-8xl px-10 py-5">
-        Under Development
+        V 1.2
       </h1>
       <h1 className=" italic underline bg-gradient-to-r font-extrabold  from-pink-700 to-purple-500 text-transparent bg-clip-text text-5xl sm:text-8xl px-10 py-5">
       Coming Soon
